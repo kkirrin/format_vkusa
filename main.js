945447ffdbcc139dfaf3cdf239de2d41f90e7bae
kkirrin <khlebnikov-2016@mail.ru>
2024-11-06 15:22:47 +1000

    git init

---
+++ main.js
@@ -0,0 +1,9 @@
+import './style.scss';
+import { init } from '/js-module/init';
+
+window.addEventListener('DOMContentLoaded', () => {
+  console.log('подключен скрипт main.js');
+
+  init();
+  // DOMContentLoaded
+});
---
+++ tailwind.config.js
@@ -0,0 +1,37 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './*.html',
+  ],
+  theme: {
+    screens: {
+      'sm': '768px',
+      'md': '1024px',
+      'lg': '1280px',
+      'xl': '1440px',
+    },
+
+    colors: {
+      transparent: 'transparent',
+      current: 'currentColor',
+      'white': '#ffffff',
+
+      // ---------------------------------
+      'default-black': '#000000',
+      'dark-blue': '#152033',
+      'blue': '#192436',
+      'orange': 'orange',
+    },
+
+    container: {
+      padding: '20px',
+      center: true
+    },
+    extend: {
+      fontFamily: {
+        'bodyFont': ['archangelsk', 'regular'],
+      }
+    }
+  },
+  plugins: [],
+}
